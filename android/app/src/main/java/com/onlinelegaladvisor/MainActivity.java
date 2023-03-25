fbaedd77ea95d4d154a9d7c293a07d5ca460f821
Damru Ravihara <damru@agrithmics.lk>
2023-03-25 12:43:38 +0530

    chane name

--- android/app/src/main/java/com/onlinelegaladvisor/MainActivity.java
+++ android/app/src/main/java/com/onlinelegaladvisor/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "OnlineLegalAdvisor";
+    return "ruhunu-life";
   }
 
   @Override
